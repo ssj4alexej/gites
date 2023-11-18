beddef9bf5c3e97be3307756160ceb52417d4be7
Alex Fischer <ssj4alexej@gmail.com>
2023-11-18 12:04:53 +0100

    202311181204

--- gites/Form1.cs
+++ gites/Form1.cs
@@ -16,5 +16,10 @@ namespace gites
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Mindentistud.VISSZAAD_VALAMIT());
+        }
     }
 }
---
+++ gites/Mindentistud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gites
+{
+    public static class Mindentistud
+    {
+        public static string VISSZAAD_VALAMIT()
+        {
+            return "valami";
+        }
+    }
+}
